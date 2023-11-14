ed59f1fdf6722ab36faa4904fb0e9e1c27759be9
root <root@DESKTOP-UL4HRG1>
2023-11-14 10:37:22 +0100

    initial commit

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the desccription of the table called first_table.
+SHOW CREATE TABLE `first_table`;
+
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Lista all the fields in the table called first_table
+SELECT *FROM `first_table`;
